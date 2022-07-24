17dc322f050dcf4f5368ca9874f877b3f3c25222
HairiBeast <shpetzla@gmail.com>
2022-07-24 21:06:49 +0100

    Completed about section. Responsive but some glitchin on certain screens (photo getting cut off) may need to change how I've approached the sizing and responsiveness.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./about.css";
+import ME2 from "../../assets/ME2.JPG";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const about = () => {
-  return <section id="about">about</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME2} alt="About Image" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>School of Code</small>
+            </article>
+
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>Charities and Creators</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>Commissioned and Personal</small>
+            </article>
+          </div>
+
+          <p>
+            Having previously worked as a teacher, tailoring lessons for
+            classes, I've become particularly interested in UX/UI to help convey
+            ideas in a way that is intuitive and accessible.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default about;
