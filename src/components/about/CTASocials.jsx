a9c346f21b7d4a2acc29015b4d36d4417e917434
HairiBeast <shpetzla@gmail.com>
2022-09-14 20:10:35 +0100

    added extra button to homepage

--- src/components/about/CTASocials.jsx
+++ src/components/about/CTASocials.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import CV from "../../assets/CV.pdf";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
+import { ImProfile } from "react-icons/im";
 
 const CTASocials = () => {
   return (
     <div className="cta-socials">
+      <div>
+        <a href={CV} download>
+          <ImProfile size={30} className="cta-icon" />
+        </a>
+        {/* <h5 className="nav-text">CV</h5> */}
+      </div>
       <div>
         <a href="www.linkedin.com/in/harri-best" target="_blank">
           <BsLinkedin size={30} className="cta-icon" />
